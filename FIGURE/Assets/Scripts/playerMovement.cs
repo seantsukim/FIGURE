a8265af512898662d62be19273728891144c430a
seantsukim <seantsukim@gmail.com>
2018-09-14 19:10:22 -0700

    made some scripts
    working on making the weapon fire correctly

--- FIGURE/Assets/Scripts/playerMovement.cs
+++ FIGURE/Assets/Scripts/playerMovement.cs
@@ -4,13 +4,48 @@ using UnityEngine;
 
 public class playerMovement : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+	public float moveSpeed = 0f;
+	public float jumpHeight = 0f;
+
+	private Rigidbody2D rb;
+	private bool isGrounded;
+
+	//Initialize variables
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody2D>();
+		isGrounded = true;
+	}
+
+	void OnCollisionEnter2D(Collision2D collided)
+	{
+		//allows for jump once u hit the ground
+		if (collided.gameObject.tag == "floor")
+		{
+			isGrounded = true;
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+
+
+	}
+
+	//Player Movement
+	void FixedUpdate()
+	{	
+		//check to move left or right
+		float axis = Input.GetAxisRaw("Horizontal");
+
+		//player jump (once)
+		if (isGrounded && (Input.GetAxisRaw("Jump") > 0))
+		{
+			rb.AddForce(Vector2.up * jumpHeight, ForceMode2D.Impulse);
+			isGrounded = false;
+		}
+
+		//horizontal movement
+		rb.velocity = new Vector3(moveSpeed * axis, rb.velocity.y, 0);
 	}
 }
---
+++ FIGURE/Assets/Scripts/weapon.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class weapon : MonoBehaviour {
+
+	public Transform fireOrigin;
+	public float fireRate = 0.5f;
+	public float bulletSpeed = 0.5f;
+	public float bulletDuration = 2.0f;
+	public Object bullet;
+
+	void Awake () {
+		
+	}
+
+	void fire()
+	{
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown("Fire1"))
+		{
+			fire();
+		}
+	}
+}
