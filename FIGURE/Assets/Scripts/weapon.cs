74923b119838dff24ad4e1468bcb7bee1e536ad1
seantsukim <seantsukim@gmail.com>
2018-10-01 14:01:02 -0700

    player char done

--- FIGURE/Assets/Scripts/weapon.cs
+++ FIGURE/Assets/Scripts/weapon.cs
@@ -4,25 +4,41 @@ using UnityEngine;
 
 public class weapon : MonoBehaviour {
 
-	public Transform fireOrigin;
-	public float fireRate = 0.5f;
-	public float bulletSpeed = 0.5f;
-	public float bulletDuration = 2.0f;
-	public Object bullet;
+	public Transform fireOrigin; //where to instantiate bullet
+	//public float fireCooldown = 0.5f;
+	public float bulletSpeed = 0.5f; //how fast the bullet should travel
+	public float rateOfFire = 0.25f; //how long to wait before firing
+	public float bulletDuration = 2.0f; //how long the bullet should last
+	public Object bullet; //the bullet object being fired
 
-	void Awake () {
-		
+	private float bulletCooldown; //bullet buffer
+
+	void Start() {
+		bulletCooldown = 0f;
 	}
 
 	void fire()
 	{
+		//Debug.Log("Pew!");
+		/*
+		Vector2 mousePos = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
+		Vector2 fireCoordinates = new Vector2(fireOrigin.transform.position.x, fireOrigin.transform.position.y);
+		RaycastHit2D hit = Physics2D.Raycast(fireCoordinates, mousePos - fireCoordinates, 1000);
+		*/
+		var newBullet = (GameObject)Instantiate(bullet, fireOrigin.position, fireOrigin.rotation);
+		newBullet.GetComponent<Rigidbody2D>().velocity = newBullet.transform.right * bulletSpeed;
+
+		Destroy(newBullet, bulletDuration);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetButtonDown("Fire1"))
+		bulletCooldown += Time.deltaTime * 1;
+
+		if (Input.GetButton("Fire1") && (bulletCooldown >= rateOfFire))
 		{
 			fire();
+			bulletCooldown = 0;
 		}
 	}
 }
